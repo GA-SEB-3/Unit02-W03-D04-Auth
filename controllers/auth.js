ab28bf1450e7ed5ab38bea78d6d754ad3588f207
omarakamal <omaraakamal@gmail.com>
2025-01-08 20:21:58 +0300

    updated code

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,7 @@
 const express = require("express")
 const router = express.Router()
+const User = require('../models/user')
+const bcrypt = require('bcrypt')
 
 router.get("/", async(req,res)=>{
     res.render("index.ejs")
@@ -7,6 +9,32 @@ router.get("/", async(req,res)=>{
 
 router.get("/sign-up",(req,res)=>{
     res.render("auth/sign-up.ejs")
+})
+
+router.post("/sign-up",async(req,res)=>{
+  console.log(req.body)
+  const userInDatabase = await User.findOne({username:req.body.username})
+
+  // check if the username is already taken
+  if(userInDatabase){
+    return res.send("Username already taken")
+  }
+
+  // check if passwords match
+  if (req.body.password !== req.body.confirmPassword) {
+    return res.send("Password and Confirm Password must match");
+  }
+
+  console.log("OLD PASSWORD: ", req.body.password)
+
+  const hashedPassword = bcrypt.hashSync(req.body.password,10)
+  console.log("NEW PASSWORD: ",hashedPassword)
+  req.body.password = hashedPassword
+
+  const createdUser = await User.create(req.body)
+
+  res.send(`thank you for signing up ${createdUser.username}`)
+
 })
 
   
